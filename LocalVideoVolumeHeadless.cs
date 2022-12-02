18c8bc7365877b256e1f0c802ae5a132c43a6f92
NeroWolf001 <118631622+NeroWolf001@users.noreply.github.com>
2022-12-02 11:43:08 -0500

    Update
    Rewrite of entire mod.

--- LocalVideoVolumeHeadless.cs
+++ LocalVideoVolumeHeadless.cs
@@ -7,24 +7,64 @@ namespace LocalVideoVolumeHeadless
     public class LocalVideoVolumeHeadless : NeosMod
     {
         public override string Name => "LocalVideoVolumeHeadless";
-        public override string Author => "NeroWolf & Sox & LeCloutPanda";
-        public override string Version => "1.0.0";
+        public override string Author => "NeroWolf & LeCloutPanda";
+        public override string Version => "2.0.0";
+
         public override void OnEngineInit()
         {
-            Harmony harmony = new Harmony("net.nero.LocalVideoPlayerVolume");
+            Harmony harmony = new Harmony($"dev.{Author}.{Name}");
             harmony.PatchAll();
+
+            Engine.Current.OnReady += Current_OnReady;
         }
-        [HarmonyPatch(typeof(VideoPlayer), "OnChanges")]
-        private class LocalVideoPlayerVolumePatchHeadless
+
+        private void Current_OnReady()
         {
-            private static void Postfix(VideoPlayer __instance, SyncRef<AudioOutput> ____mainAudioOutput)
+            Engine.Current.WorldManager.WorldAdded += WorldAdded;
+            Engine.Current.WorldManager.WorldRemoved += WorldRemoved;
+        }
+
+        private void WorldAdded(World obj) => obj.ComponentAdded += OnComponentAdded;
+        private void WorldRemoved(World obj) => obj.ComponentAdded -= OnComponentAdded;
+
+        private void OnComponentAdded(Slot arg1, Component arg2)
+        {
+            if (arg2.GetType() == typeof(AudioOutput))
             {
-                ValueUserOverride<float> valueUserOverride = __instance.Slot.AttachComponent<ValueUserOverride<float>>(true, null);
-                valueUserOverride.CreateOverrideOnWrite.Value = true;
-                valueUserOverride.Target.Target = ____mainAudioOutput.Target.Volume;
-                valueUserOverride.Default.Value = 0f;
+                VideoPlayer videoPlayer = arg1.GetComponent<VideoPlayer>();
+                if (videoPlayer == null) return;
+
+                arg1.RunInUpdates(3, () =>
+                {
+                    AudioOutput audioOutput = (AudioOutput)arg2;
+
+                    ValueUserOverride<float> userOverride = arg1.GetComponent<ValueUserOverride<float>>();
+
+                    if (userOverride == null)
+                    {
+                        ValueUserOverride<float> valueUserOverride = arg1.AttachComponent<ValueUserOverride<float>>(true, null);
+                        valueUserOverride.CreateOverrideOnWrite.Value = true;
+                        valueUserOverride.Target.Target = audioOutput.Volume;
+                        valueUserOverride.Default.Value = 0f;
+                    }
+                    else if (userOverride != null)
+                    {
+                        userOverride.Default.Value = 0;
+                        audioOutput.Volume.Value = 0f;
+                    }
+
+                    Slot volume = arg1.FindChild(ch => ch.Name.Equals("Volume"), 1);
+                    TextRenderer text = volume.AddSlot("Local Text").AttachComponent<TextRenderer>();
+                    text.Text.Value = "Local";
+                    text.Slot.Scale_Field.Value = new BaseX.float3(0.5f, 0.5f, 0.5f);
+                    text.Slot.Position_Field.Value = new BaseX.float3(0f, 0f, -0.02f);
+                });
             }
         }
     }
 }
-    
+
+
+
+
+
